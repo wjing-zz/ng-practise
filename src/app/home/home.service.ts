e1f227e7e2230b59db58d076ee662bca60ed4394
jing.g.wang <jing.g.wang@accenture.com>
2023-05-31 11:25:07 +0800

    feat: rename class

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -8,14 +8,7 @@ export class HomeService {
   constructor(private http: HttpClient) { }
 
   getHCPList() {
-    //return 'assets/mockdata/hcp.data.json';
     return this.http.get('assets/mockdata/hcp.data.json');
   }
 
-  getSchedule(period: any) {
-    const scheduleCount = [{
-      contacts: { toDo: 2, schedule: 2, done: 1 },
-      meeting: 2
-    }]
-  }
 }
